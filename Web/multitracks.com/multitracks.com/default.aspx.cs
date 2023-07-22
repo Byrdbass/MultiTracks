938daf3031c3bd9d9712eb54d2344a50b80fc036
ByrdBassPC <byrdbass@gmail.com>
2023-07-22 14:15:07 -0500

    finished setting Lists for ArtistDetails to account for multiple rows coming back from Query in Stored Procedure

--- Web/multitracks.com/multitracks.com/default.aspx.cs
+++ Web/multitracks.com/multitracks.com/default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccess;
 using System;
+using System.Collections.Generic;
 using System.Data;
 
 public partial class Default : MultitracksPage
@@ -8,6 +9,23 @@ public partial class Default : MultitracksPage
     protected string Img { get; set; }
     protected string HeroImg { get; set; }
     protected string Bio { get; set; }
+    protected List<int> AlbumIds { get; set; }
+    protected List<string> AlbumTitles { get; set; }
+    protected List<string> AlbumImages { get; set; }
+    protected List<int> AlbumYears { get; set; }
+    protected List<int> SongIds { get; set; } 
+    protected List<DateTime> SongDates { get; set; }
+    protected List<string> SongTitles { get; set; }
+    protected List<decimal> BPMs { get; set; }
+    protected List<string> TimeSignatures { get; set; }
+    protected List<bool> Multitracks { get; set; }
+    protected List<bool> CustomMixes { get; set; }
+    protected List<bool> Charts { get; set; }
+    protected List<bool> RehearsalMixes { get; set; }
+    protected List<bool> Patches { get; set; }
+    protected List<bool> ProPresenters { get; set; }
+
+
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -44,6 +62,62 @@ public partial class Default : MultitracksPage
             HeroImg = data.Rows[0]["HeroImg"].ToString();
             Bio = data.Rows[0]["Bio"].ToString();
 
+            //using new keyword to clear data before next search
+            AlbumIds = new List<int>();
+            AlbumTitles = new List<string>();
+            AlbumImages = new List<string>();
+            AlbumYears = new List<int>();
+
+            SongIds = new List<int>();
+            SongDates = new List<DateTime>();
+            SongTitles = new List<string>();
+            BPMs = new List<decimal>();
+            TimeSignatures = new List<string>();
+            Multitracks = new List<bool>();
+            CustomMixes = new List<bool>();
+            Charts = new List<bool>();
+            RehearsalMixes = new List<bool>();
+            Patches = new List<bool>();
+            ProPresenters = new List<bool>();
+
+            foreach (DataRow row in data.Rows)
+            {
+                int albumId = Convert.ToInt32(row["AlbumId"]);
+                string albumTitle = row["AlbumTitle"].ToString();
+                string albumImage = row["AlbumImage"].ToString();
+                int year = Convert.ToInt32(row["AlbumYear"]);
+
+                int songId = Convert.ToInt32(row["SongId"]);
+                DateTime dateCreation = Convert.ToDateTime(row["SongDate"]);
+                string songTitle = row["SongTitle"].ToString();
+                decimal bpm = Convert.ToDecimal(row["BPM"]);
+                string timeSignature = row["TS"].ToString();
+                bool multitrack = Convert.ToBoolean(row["MT"]);
+                bool customMix = Convert.ToBoolean(row["Mix"]);
+                bool chart = Convert.ToBoolean(row["Chart"]);
+                bool rehearsalMix = Convert.ToBoolean(row["RM"]);
+                bool patch = Convert.ToBoolean(row["patches"]);
+                bool proPresenter = Convert.ToBoolean(row["Pro"]);
+                
+
+                AlbumIds.Add(albumId);
+                AlbumTitles.Add(albumTitle);
+                AlbumImages.Add(albumImage);
+                AlbumYears.Add(year);
+
+                SongIds.Add(songId);
+                SongDates.Add(dateCreation);
+                SongTitles.Add(songTitle);
+                BPMs.Add(bpm);
+                TimeSignatures.Add(timeSignature);
+                Multitracks.Add(multitrack);
+                CustomMixes.Add(customMix);
+                Charts.Add(chart);
+                RehearsalMixes.Add(rehearsalMix);
+                Patches.Add(patch);
+                ProPresenters.Add(proPresenter);
+                
+            }
 
         }
     }
